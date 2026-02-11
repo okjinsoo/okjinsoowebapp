1d99a2c31879268f56a1f959377941424807b755
okjinsoo <rapah0310@gmail.com>
2026-02-11 16:23:44 +0900

    Make all-in-one SQL bundle rerunnable for snapshot policies

--- db/combined/001_008_all_in_one.sql
+++ db/combined/001_008_all_in_one.sql
@@ -487,6 +487,26 @@ BEGIN
   ) THEN
     DROP POLICY app_state_snapshots_update_all_authenticated ON public.app_state_snapshots;
   END IF;
+
+  IF EXISTS (
+    SELECT 1
+    FROM pg_policies
+    WHERE schemaname = 'public'
+      AND tablename = 'app_state_snapshots'
+      AND policyname = 'app_state_snapshots_write_main_authenticated'
+  ) THEN
+    DROP POLICY app_state_snapshots_write_main_authenticated ON public.app_state_snapshots;
+  END IF;
+
+  IF EXISTS (
+    SELECT 1
+    FROM pg_policies
+    WHERE schemaname = 'public'
+      AND tablename = 'app_state_snapshots'
+      AND policyname = 'app_state_snapshots_update_main_authenticated'
+  ) THEN
+    DROP POLICY app_state_snapshots_update_main_authenticated ON public.app_state_snapshots;
+  END IF;
 END;
 $$;
 
